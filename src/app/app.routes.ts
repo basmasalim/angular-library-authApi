0ad04d9a52763bd7394f915b6af9024001f221a1
basmasalim <basmasalim030@gmail.com>
2025-06-13 08:17:32 +0300

    login ui and install angular library

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadComponent: () => import('./core/pages/login/login.component').then(m => m.LoginComponent)},
+    // {path: 'register', loadComponent: () => import('./core/pages/register/register.component').then(m => m.RegisterComponent)},
+];
